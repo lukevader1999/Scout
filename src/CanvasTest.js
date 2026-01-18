474f0c459d6b071a7ad23a95cefda847b2a4d433
Erik <ewalter@students.uni-mainz.de>
2026-01-18 15:54:51 +0100

    First rendered hand

--- src/CanvasTest.js
+++ src/CanvasTest.js
@@ -1,15 +1,47 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+const ctx = document.getElementById("canvas").getContext("2d");
+const CARD_WIDTH = 150
+const highlightedCardsIndices = []
 
-ctx.fillStyle = "green";
-ctx.fillRect(10, 10, 150, 100);
-
-function drawPicture(ctx, path, ...args) {
+function drawPictureWithFixedWidth(path, x, y, scaledWidth) {
     const img = new Image();
-    img.onload = function () {
-        ctx.drawImage(img, ...args)
-    }
-    img.src = "/home/erik/GitRepos/Scout/assets/images/cards/45.jpg"
+    img.src = path
+    img.onload = () => {
+        ctx.drawImage(img, x, y, scaledWidth, scaledHeight(img, scaledWidth));
+    };
 }
 
-drawPicture(ctx, "", 0, 0)
+function imagePath(id) {
+    return "./assets/images/cards/raw/" + id.toString() + ".jpg"
+}
+
+function scaledHeight(img, scaledWidth) {
+    return img.naturalHeight * scaledWidth / img.naturalWidth
+}
+
+function drawBorder(x, y, width, height) {
+    const LINE_WIDTH = 5
+    ctx.lineWidth = LINE_WIDTH
+    ctx.strokeRect(x, y, width, height)
+}
+
+function drawCard(id, x, y) {
+    const img = new Image()
+    img.src = imagePath(id)
+    img.onload = () => {
+        const CARD_HEIGHT = scaledHeight(img, CARD_WIDTH)
+        ctx.drawImage(img, x, y, CARD_WIDTH, CARD_HEIGHT)
+    drawBorder(x, y, CARD_WIDTH, CARD_HEIGHT)
+    }
+}
+
+function drawCards(ids, x, y) {
+    const offset = CARD_WIDTH + 8
+    let movingX = x
+    for(let id of ids) {
+        console.log(id)
+        drawCard(id, movingX, y)
+        movingX += offset
+    }
+}
+
+drawCards(["38", "23", "510", "510" , "510", "510"], 80, 500 )
